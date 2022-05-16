30cc86ebaf1ec052a5186f94380be23d1d5b9cab
Nick Adams <nick.pappas.adams@gmail.com>
2022-05-16 18:32:57 -0500

    added test for null value

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,7 @@ test("adds 2 + 2 to equal 4", () => {
 test("adds 2 + 2 to not equal 5", () => {
     expect(functions.add(2, 2,)).not.toBe(5)
 })
+
+test("should be null", () => {
+    expect(functions.isNull()).toBeNull()
+})
